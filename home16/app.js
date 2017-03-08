23bf571ccd37cd083cce54e62b4d674169a9e85f
Wolf <volkov911@tut.by>
2017-03-08 14:39:36 +0300

    JS homework

--- home16/app.js
+++ home16/app.js
@@ -1,3 +1,25 @@
-/**
- * Created by Slava on 08.03.2017.
- */
+var bInput = prompt('Введите фамилию'),
+    aInput = prompt('Введите имя'),
+    cInput = prompt('Введите отчество'),
+    ageInput = Number(prompt('Введите возраст')),
+    d = ageInput * 365,
+    gender = confirm('Выберите пол: ок - муж, отмена - жен') ? 'муж' : 'жен',
+    c = ageInput + 5;
+alert('Ваше ФИО: ' + bInput + ' ' + aInput + ' ' + cInput + '\nваш возраст в годах: ' + ageInput + '\nваш возраст в днях: ' + d
+    + '\nчерез 5 лет вам будет: ' + c + '\nваш пол: ' + gender);
+
+var gender1 = confirm('Ваш пол мужской?');
+if (gender1) {
+    gender1 = 'муж';
+    /*alert('Ваше ФИО: ' + bInput + ' ' + aInput + ' ' + cInput + '\nваш возраст в годах: ' + ageInput + '\nваш возраст в днях: ' + d
+        + '\nчерез 5 лет вам будет: ' + c + '\nваш пол: ' + 'муж');*/
+}
+else {
+    gender1 = 'жен';
+    /*alert('Ваше ФИО: ' + bInput + ' ' + aInput + ' ' + cInput + '\nваш возраст в годах: ' + ageInput + '\nваш возраст в днях: ' + d
+        + '\nчерез 5 лет вам будет: ' + c + '\nваш пол: ' + 'жен')*/
+}
+alert('Ваше ФИО: ' + bInput + ' ' + aInput + ' ' + cInput + '\nваш возраст в годах: ' + ageInput + '\nваш возраст в днях: ' + d
+    + '\nчерез 5 лет вам будет: ' + c + '\nваш пол: ' + gender1);
+
+
